bc49186a043f6aff6add84741d26c3cdee75d209
cy <darcyz@163.com>
2023-05-25 17:05:04 +0800

    111

--- src/main/java/com/example/smp/controller/test/Main.java
+++ src/main/java/com/example/smp/controller/test/Main.java
@@ -1,11 +1,20 @@
 package com.example.smp.controller.test;
 
+import org.codehaus.xfire.client.Client;
+
+import java.net.URL;
+
 public class Main {
 
-    public static void main(String[] args) {
-        int total = 12;
-        int pageSize = 5;
-        int ceil = (int) Math.ceil((double)total/(double)pageSize);
-        System.out.println(ceil);
+    public static void main(String[] args) throws Exception {
+//        int total = 12;
+//        int pageSize = 5;
+//        int ceil = (int) Math.ceil((double)total/(double)pageSize);
+//        System.out.println(ceil);
+
+        Client client = new Client(new URL("http://127.0.0.1:9990/configcenter/services/configurationService?wsdl"));
+        Object[] ob = client.invoke("getTheSystemAllConfigurations", new Object[]{"globalConfig", "dev"});
+        System.out.println(ob);
+
     }
 }
